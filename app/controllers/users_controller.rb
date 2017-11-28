c1f9d667ef94f64007ad87b895ce2a47752c6891
westondombroski <westondombroski@gmail.com>
2017-11-27 19:09:01 -0800

    linked user show page to user email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
     before_filter :authenticate_user!
     
+      def index
+        @user = User.find(params[:id])
+      end
+
       def show
         @user = User.find(params[:id])
     
