19a594dd4da15f4d7d8452196d01ab1d4f621503
westondombroski <westondombroski@gmail.com>
2017-12-02 18:53:32 -0800

    search animation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
     before_action :authenticate_user!, only: [:new, :edit, :destroy]
     before_action :is_owner, only: [:edit, :destroy]
     def index
-        @posts = Post.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 2)
+        @posts = Post.where(["title LIKE ?", "%#{params[:search]}%"]).order("created_at DESC").paginate(:page => params[:page], :per_page => 2)
     end
 
     def show 
